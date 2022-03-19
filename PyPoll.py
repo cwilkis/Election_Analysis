80b232ab276483c32381fef510d231953b4f864f
claudia wilkis <claudiajwilkis@gmail.com>
2022-03-19 14:22:19 -0500

    psedo code

--- PyPoll.py
+++ PyPoll.py
@@ -1,15 +1,6 @@
-print("Hello World")
-
-#How many votes did you get?
-my_votes = int(input("How many votes did you get in the election? "))
-
-#Total votes in the election
-total_votes = int(input("what is the total votes in the election? "))
-
-#Calculate the percentage of votes you received
-percentage_votes = (my_votes / total_votes) * 100
-
-print("I received " + str(percentage_votes)+"% of the total votes. ")
-
-
-
+# The data we need to retrieve
+# 1. The total number of votes cast
+# 2. Complete list of candidates who received votes
+# 3. The total number of votes each candidate won
+# 4. The percentage of votes each candidate won
+# 5. The winner of the election based on popular vote
